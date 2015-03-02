7ddc9994dda022abe79c51be7d8ce80918bed732
okunoryo <okuno.ryo.411@gmail.com>
2015-03-03 00:25:04 +0900

    リファクタリング

--- stopMap/stop_point.py
+++ stopMap/stop_point.py
@@ -2,14 +2,13 @@
 #-------------------------
 #
 # excelからデータ呼び出し
-# 燃費データの算出
+# jsonに変換
 #
 #-------------------------
 
 import xlrd
 import time
 import json
-import time
 
 # 読み込みファイルの指定
 book = xlrd.open_workbook('../logs/0223BMW116i.xlsx')
@@ -17,7 +16,7 @@ book = xlrd.open_workbook('../logs/0223BMW116i.xlsx')
 # シートの指定(summary)
 sheet_1 = book.sheet_by_index(0)
 
-# セルデータの登録
+# セルデータ呼び出し
 log_date = 2
 log_time = 3
 log_record_type = 4
@@ -33,9 +32,7 @@ log_engin_status = 14
 line = "====================="
 
 if __name__ == "__main__":
-
     stop_point = []
-    print "今回の走行"
     # 止まっている状態のGPS情報を取得
     for row in range(sheet_1.nrows):
         if sheet_1.cell(row,log_speed).value < 10 and sheet_1.cell(row,log_record_type).value == "GpsRecord":
@@ -46,8 +43,9 @@ if __name__ == "__main__":
                     "point":point_get,
                    }
             stop_point.append(json_point)
-    f = open("log_data.json", "w")            
-    print stop_point
-    f.write(str(json.dumps(stop_point)))
-    
+            print json_point
+    # jsonファイル作成
+    f = open("log_data.json", "w")
+    f.write(str(json.dumps(stop_point)))    
     f.close()
+    print "created. => log_data.json"
